c62f12db7c827d362830a13ce4ecfac21f54d20c
Zenafey <zenafey@eugw.ru>
2025-06-24 07:48:48 +0500

    eslint

--- frontend/todo-nextjs/eslint.config.mjs
+++ frontend/todo-nextjs/eslint.config.mjs
@@ -13,7 +13,9 @@ const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript"],
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off'
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'react/no-unescaped-entities': 'off'
     },
   })
 ];
