f10a5b91564ef87e40428999d29b87c70fd39133
Zenafey <zenafey@eugw.ru>
2025-06-25 16:04:19 +0500

    fix

--- frontend/todo-nextjs/next.config.ts
+++ frontend/todo-nextjs/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['127.0.0.1', '26.64.197.106'],
+  allowedDevOrigins: ['127.0.0.1', '26.64.197.106', '46.148.238.212'],
   devIndicators: false,
   ignoreDuringBuilds: true,
 };
